7526e7e773a31c4ed5001e65f8c5c88b0452d096
keemhyojin <gywls125@gmail.com>
2025-04-12 17:16:23 +0900

    충돌처리

--- 250411_WinAPI (1)/250304_WinAPI/MainGame.h
+++ 250411_WinAPI (1)/250304_WinAPI/MainGame.h
@@ -1,33 +1,3 @@
-//#pragma once
-//#include "config.h"
-//class Timer;
-//class Image;
-//class TilemapTool;
-//class MainGame
-//{
-//private:
-//	HDC hdc;
-//	PAINTSTRUCT ps;
-//	wchar_t szText[128];
-//
-//	Image* backBuffer;
-//
-//	TilemapTool* tilemapTool;
-//
-//	Timer* timer;
-//
-//public:
-//	HRESULT Init();
-//	void Release();
-//	void Update();
-//	void Render();
-//
-//	LRESULT MainProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
-//
-//	MainGame();
-//	~MainGame();
-//};
-
 #pragma once
 #include "config.h"
 class Timer;
